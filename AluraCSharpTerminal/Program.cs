3f7a0a418da0451b26bfc3b67e6847cd6328180d
Camila Silva <camilaibs@gmail.com>
2015-11-08 23:40:15 -0200

    Projeto

---
+++ AluraCSharpTerminal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraCSharpTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TextReader leitor = Console.In;
+            //string linha = leitor.ReadLine();
+            string linha = Console.ReadLine();
+            while (linha != null)
+            {
+                Console.WriteLine("Linha: " + linha);
+                //linha = leitor.ReadLine();
+                linha = Console.ReadLine();
+            }
+            Console.WriteLine("Pressionado Ctrl + Z");
+        }
+    }
+}
